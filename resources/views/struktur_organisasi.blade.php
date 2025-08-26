4321246233ce8115748af5a3b44b28fd352fe10d
Bagood25 <bagus.tri250904@gmail.com>
2025-08-26 08:52:01 +0700

    Update isi repo

--- resources/views/struktur_organisasi.blade.php
+++ resources/views/struktur_organisasi.blade.php
@@ -3,12 +3,19 @@
 @section('title', 'Struktur Organisasi')
 
 @section('content')
-    <div class="bg-gray-900 py-16 px-4 text-center text-white">
-        <h1 class="text-4xl font-extrabold sm:text-5xl">Struktur Organisasi</h1>
-        <p class="mt-2 text-lg text-gray-300">
-            <a href="{{ url('/') }}" class="hover:underline">Beranda</a> &gt; 
-            <span class="font-medium">Struktur Organisasi</span>
-        </p>
+    {{-- Bagian Header dengan Latar Belakang Gambar Hutan --}}
+    <div class="relative py-16 px-4 text-center text-white" style="background-image: url('{{ asset('images/background hutan.jpg') }}'); background-size: cover; background-position: center;">
+        {{-- Lapisan Overlay Gelap untuk Keterbacaan Teks --}}
+        <div class="absolute inset-0 bg-black opacity-50"></div>
+        
+        {{-- Konten Teks di Atas Overlay --}}
+        <div class="relative z-10">
+            <h1 class="text-4xl font-extrabold sm:text-5xl">Struktur Organisasi</h1>
+            <p class="mt-2 text-lg text-gray-300">
+                <a href="{{ url('/') }}" class="hover:underline">Beranda</a> &gt; 
+                <span class="font-medium">Struktur Organisasi</span>
+            </p>
+        </div>
     </div>
 
     <main class="py-12 bg-gray-50">
